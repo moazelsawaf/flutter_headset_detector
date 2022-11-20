8bd6bc73cdd4836f3494e85390cba00c8e0cce8d
Nour Magdi <snowpiercer2001@gmail.com>
2022-11-20 11:10:28 +0200

    Update FlutterHeadsetDetectorPlugin.java

--- android/src/main/java/flutter/moum/flutter_headset_detector/FlutterHeadsetDetectorPlugin.java
+++ android/src/main/java/flutter/moum/flutter_headset_detector/FlutterHeadsetDetectorPlugin.java
@@ -133,6 +133,8 @@ public class FlutterHeadsetDetectorPlugin implements FlutterPlugin, MethodCallHa
 
         for (AudioDeviceInfo deviceInfo : audioDevices) {
             int deviceType = deviceInfo.getType();
+            System.out.println("[JAVA: bluetooth type] ");
+            System.out.println(deviceType);
             if (deviceType == AudioDeviceInfo.TYPE_BLUETOOTH_A2DP || deviceType == AudioDeviceInfo.TYPE_BLUETOOTH_SCO ||
                     deviceType == AudioDeviceInfo.TYPE_BLE_HEADSET || deviceType == AudioDeviceInfo.TYPE_BLE_SPEAKER) {
                 return 1;
